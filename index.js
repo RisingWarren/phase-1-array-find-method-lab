166be8352c9b41e9600844c0bb962cc84703d233
Warren Santiago <wsantiago9623@gmail.com>
2022-10-11 13:39:58 -0400

    commit tree 193b7399b473f5d4e72a924b82a90d464affefdd

--- index.js
+++ index.js
@@ -1,1 +1,52 @@
 // code your solution here
+
+function superbowlWin(record){
+   
+const recWin = record.find(object => object.result === "W")
+
+if (recWin === undefined){
+    return undefined} else{
+    return recWin.year}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const recordedWin = record.find(object => object.result === "W");
+
+    
+
+// console.log(recordedWin) 
+
+// if(recordedWin === undefined){
+//     return undefined
+// } else {
+//     return recordedWin.year
+// } 
